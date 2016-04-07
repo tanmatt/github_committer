c0728f13312608af988e1ec14a6a5024ff0beadf
tanmayzyp <tthakur@zypmedia.com>
2016-04-07 16:22:02 -0700

    Logging for randomness

--- randomness.py
+++ randomness.py
@@ -5,11 +5,21 @@ crontab at 23:59:59 everyday
 import random
 import subprocess
 import os
+import time
 
 home_dir = subprocess.check_output(["pwd"]).strip()
 project_dir = home_dir + '/github_committer/'
 os.chdir(project_dir)
 FILENAME = project_dir + 'todays_random_hours.txt'
+LOGFILE = project_dir + 'events.log'
+
+TIME = time.gmtime()
+log_message = "\n" + str(time.strftime("%Y-%m-%d:", TIME)) + "\t"
+
+
+def _flush_log():
+    with open(LOGFILE, 'a') as f:
+        f.write(log_message)
 
 
 def write_to_file(hour_list):
@@ -21,6 +31,8 @@ def write_to_file(hour_list):
 def get_randoms():
     hours_count = random.randint(1, 7)
     print "hours_count", hours_count
+    global log_message
+    log_message += str(hours_count) + " commits will be done today\t"
     hours_list = []
 
     for i in range(hours_count):
@@ -28,6 +40,7 @@ def get_randoms():
 
     print "hours_list", hours_list
     write_to_file(hours_list)
+    log_message += "And it will run on hours = " + str(hours_list)
 
 
 if __name__ == '__main__':
