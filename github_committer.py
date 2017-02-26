ddbbe8cdc40a01e3b78f967cdbff289ea43abd81
tanmaythakur <tanmaythakur2@gmail.com>
2017-02-26 10:59:07 -0600

    Updated with new file on 16

--- github_committer.py
+++ github_committer.py
@@ -39,7 +39,7 @@ def download_xls():
     try:
         #os.rename(DOWNLOADED_FILE, put_in_dir + DOWNLOADED_FILE)
         print os.getcwd()
-        os.remove("rank_1(1).xls")
+        os.remove("rank_1 (1).xls")
         log_message += "Downloaded file for today\t"
     except Exception, ex:
         log_message += "Error moving the file: " + str(ex)
