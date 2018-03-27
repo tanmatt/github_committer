3b02284390f80457c17e0e1a46cd7cf92cac1152
Tanmay Thakur rpi <tanmaythakur2@gmail.com>
2018-03-26 23:18:09 -0500

    Updated with new file on 4

--- randomness.py
+++ randomness.py
@@ -10,15 +10,9 @@ import time
 project_dir = subprocess.check_output(["pwd"]).strip()
 os.chdir(project_dir)
 FILENAME = project_dir + '/todays_random_hours.txt'
-LOGFILE = project_dir + 'events.log'
 
 TIME = time.gmtime()
 log_message = "\n" + str(time.strftime("%Y-%m-%d:", TIME)) + "\t"
-
-
-def _flush_log():
-    with open(LOGFILE, 'a') as f:
-        f.write(log_message)
 
 
 def write_to_file(hour_list):
@@ -29,8 +23,6 @@ def write_to_file(hour_list):
 def get_randoms():
     hours_count = random.randint(1, 7)
     print "hours_count", hours_count
-    global log_message
-    log_message += str(hours_count) + " commits will be done today\t"
     hours_list = []
 
     for i in range(hours_count):
@@ -39,8 +31,7 @@ def get_randoms():
 
     print "hours_list" + str(hours_list)
     write_to_file(hours_list)
-    log_message += "And it will run on hours = " + str(hours_list)
-
+    
 
 if __name__ == '__main__':
     get_randoms()
