b2cc089780a15326882e3b7c516a49c2ec7182b4
Tanmay Thakur rpi <tanmaythakur2@gmail.com>
2018-04-25 21:36:55 -0500

    Updated with new file on 21

--- github_committer.py
+++ github_committer.py
@@ -22,29 +22,29 @@ GIT_BRANCH = 'zacks'
 def is_downloadable():
     # return true of --force parameter is passed from command line
     if len(sys.argv) > 1 and sys.argv[1] == "--force":
-        print "\nForcing the file download because of --force parameter"
+        print("\nForcing the file download because of --force parameter")
         return True
 
     # else check if the current hour matches with the file content
     downloadable_hours = []
-    with file(FILENAME, "r") as random_hours:
+    with open(FILENAME, "r") as random_hours:
         hours = random_hours.readlines()
         for hour in hours:
             downloadable_hours.append(hour.strip())
 
-    print "Current Hour: " + HOUR
-    print "Downloadable Hours: " + str(downloadable_hours)
+    print("Current Hour: " + HOUR)
+    print("Downloadable Hours: " + str(downloadable_hours))
 
     if HOUR in downloadable_hours:
-        print "File should be downloaded"
+        print("File should be downloaded")
         return True
     else:
-        print "File should not be downloaded. Not the right time."
+        print("File should not be downloaded. Not the right time.")
         return False
 
 
 def download_xls():
-    print "Downloading of xls started..."
+    print("Downloading of xls started...")
     # check / create folders
     put_in_dir = "xlsfiles/" + YEAR + "/" + MONTH + "/" + DATE + "/"
 
@@ -53,9 +53,9 @@ def download_xls():
             if not os.path.isdir(put_in_dir):
                 os.makedirs(put_in_dir)
 
-            print "Downloading file now..."
+            print("Downloading file now...")
             wget.download(DOWNLOAD_LINK)
-            print "\rDownloading file now...Done"
+            print("\rDownloading file now...Done")
             time.sleep(5)
 
             # remove file if it was already downloaded in the dir
@@ -64,36 +64,36 @@ def download_xls():
 
             os.rename(DOWNLOADED_FILE, put_in_dir + DOWNLOADED_FILE)
             time.sleep(2)
-            print os.getcwd()
+            print(os.getcwd())
             # remove duplicate file from project root
             if os.path.isfile("rank_1.xls"):
                 os.remove("rank_1.xls")
             _upload_to_git()
-            print "\rDownloading of xls started...Done"
-    except Exception, ex:
-        raise ex
+            print("\rDownloading of xls started...Done")
+    except(Exception):
+        raise Exception
 
 
 def _upload_to_git():
     try:
-        print "Uploading to git..."
+        print("Uploading to git...")
         # send it back to github
         os.system("git add -A")
         os.system("git commit -m \"Updated with new file on " + HOUR + "\" ")
         os.system("git push origin " + GIT_BRANCH)
-        print "\rUploading to git...Done"
-    except Exception, ex:
-        raise ex
+        print("\rUploading to git...Done")
+    except(Exception):
+        raise Exception
 
 
 def do_the_magic():
     try:
-        print "Doing the magic..."
+        print("Doing the magic...")
         # confirm the correct branch
         os.system("git checkout " + GIT_BRANCH)
         download_xls()
-    except Exception, ex:
-        raise ex
+    except(Exception):
+        raise Exception
 
 
 def main():
@@ -102,11 +102,11 @@ def main():
     :return:
     """
     try:
-        print "Program execution started...."
+        print("Program execution started....")
         do_the_magic()
-        print "Program execution finished..."
-    except Exception, ex:
-        print "Program execution failed..." + str(ex)
+        print("Program execution finished...")
+    except(Exception):
+        print("Program execution failed..." + str(ex))
 
 
 if __name__ == '__main__':
